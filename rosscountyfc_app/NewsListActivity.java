7e7f39d3a49fe36c72d5c68d4477887a2089aac5
ECjdb <49940637+ECjdb@users.noreply.github.com>
2020-01-20 19:25:06 +0000

    Update NewsListActivity.java

--- rosscountyfc_app/NewsListActivity.java
+++ rosscountyfc_app/NewsListActivity.java
@@ -62,7 +62,7 @@ public class NewsListActivity extends AppCompatActivity
     private void parseJSON()
     {
         Retrofit retrofit  = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.9/rcfcapp/v1/")
+                .baseUrl("")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
