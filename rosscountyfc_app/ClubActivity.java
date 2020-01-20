bfb035b41792e068e203fc0a14cbd432fbcc8b1b
ECjdb <49940637+ECjdb@users.noreply.github.com>
2020-01-20 19:21:30 +0000

    Update ClubActivity.java

--- rosscountyfc_app/ClubActivity.java
+++ rosscountyfc_app/ClubActivity.java
@@ -62,7 +62,7 @@ public class ClubActivity extends AppCompatActivity
         //creates a new retrofit2 object, sets the base URL and adds the GSON convertor
         //factory which is responsible for parsing the JSON response.
         Retrofit retrofit  = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.9/rcfcapp/v1/")
+                .baseUrl("")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
