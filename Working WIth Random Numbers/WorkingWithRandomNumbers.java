f3c60377c52f3d0b7b2c2584957c90459ded5298
ECjdb <49940637+ECjdb@users.noreply.github.com>
2020-09-01 18:22:21 +0100

    Add files via upload

---
+++ Working WIth Random Numbers/WorkingWithRandomNumbers.java
@@ -0,0 +1,15 @@
+package working.with.random.numbers;
+
+/**
+ *
+ * @author EC1524329
+ */
+public class WorkingWithRandomNumbers 
+{
+
+    public static void main(String[] args) 
+    {
+ 
+    }
+    
+}
